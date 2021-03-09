b8210c7aff1f740731c7ac62d3583726521b342e
André Kanayama <andre.spanguero@gmail.com>
2021-03-09 20:41:35 -0300

    Ad refresh token migration

---
+++ db/migrations/20210309094351-create-refresh-token.js
@@ -0,0 +1,44 @@
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+      return queryInterface.createTable('tokens', {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        user_id: {
+          allowNull: false,
+          type: Sequelize.INTEGER,
+          references: {
+            model: 'users',
+            key: 'id',
+          },
+        },
+        token: {
+          allowNull: false,
+          type: Sequelize.STRING,
+          unique: true,
+        },
+        valid: {
+          allowNull: false,
+          type: Sequelize.BOOLEAN,
+        },
+        expiresAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+      })
+    },
+    down: (queryInterface, Sequelize) => {
+      return queryInterface.dropTable('tokens')
+    },
+  }
