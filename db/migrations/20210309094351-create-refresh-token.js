3e05849c404f2524794e8d79d990c5a4324f63fb
André Kanayama <andre.spanguero@gmail.com>
2021-03-09 21:51:23 -0300

    Add user seed

--- db/migrations/20210309094351-create-refresh-token.js
+++ db/migrations/20210309094351-create-refresh-token.js
@@ -1,44 +1,44 @@
 module.exports = {
-    up: (queryInterface, Sequelize) => {
-      return queryInterface.createTable('tokens', {
-        id: {
-          allowNull: false,
-          autoIncrement: true,
-          primaryKey: true,
-          type: Sequelize.INTEGER,
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('tokens', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id',
         },
-        user_id: {
-          allowNull: false,
-          type: Sequelize.INTEGER,
-          references: {
-            model: 'users',
-            key: 'id',
-          },
-        },
-        token: {
-          allowNull: false,
-          type: Sequelize.STRING,
-          unique: true,
-        },
-        valid: {
-          allowNull: false,
-          type: Sequelize.BOOLEAN,
-        },
-        expiresAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-        },
-        createdAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-        },
-        updatedAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-        },
-      })
-    },
-    down: (queryInterface, Sequelize) => {
-      return queryInterface.dropTable('tokens')
-    },
-  }
+      },
+      token: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true,
+      },
+      valid: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN,
+      },
+      expiresAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    })
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('tokens')
+  },
+}
---
+++ db/seeders/20210309094351-populate-users.js
@@ -0,0 +1,27 @@
+const bcrypt = require('bcrypt')
+const { crypto: config } = require('../../config')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const users = [{
+      name: 'Teste user',
+      email: 'teste@user.com',
+      password: await bcrypt
+        .hash('1234', config.hashSaltRounds),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }, {
+      name: 'Teste admin',
+      email: 'teste@admin.com',
+      password: await bcrypt
+        .hash('1234', config.hashSaltRounds),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }]
+    await queryInterface.bulkInsert('users', users, {})
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('users', null, {})
+  },
+}
